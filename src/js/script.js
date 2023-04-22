7270ff6f6a35a7dd9fce65cc4e194b40cc887027
Michał Dobrzeniecki <dobbrzenie@gmail.com>
2023-04-22 10:47:44 +0200

    Add 8.4 js code changes

--- src/js/script.js
+++ src/js/script.js
@@ -54,21 +54,53 @@
 
   // CLASS PRODUCT CREATE
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
-
+      
+      thisProduct.id = id;
+      thisProduct.data = data;
+      
+      thisProduct.renderInMenu();
+      
       console.log('new Product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      
+      /* generate HTML basem on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      console.log(generatedHTML);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
 
   const app = {
-    initMenu: function (){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
-    init: function () {
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
@@ -76,6 +108,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
